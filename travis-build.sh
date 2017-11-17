1da5199fbd400ffa143b78f3b3a15a2f3cd4764a
Felix Faerber <ffaerber@gmail.com>
2017-11-17 21:44:26 +0100

    set chmod

--- travis-build.sh
+++ travis-build.sh
@@ -9,7 +9,7 @@ curl -fsSL https://get.docker.com/ | sudo sh
 docker version
 
 if [ "$ARCH" == "amd64" ]; then
-  docker build -t ffaerber/appsample .
+  docker build -t ffaerber/appsample --build-arg ARCH=amd64 .
 fi
 
 if [ "$ARCH" == "arm32v7" ]; then
