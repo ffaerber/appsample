240deddd67e27a6bd7ef54e81c317c0bd5eb5e11
Felix Faerber <ffaerber@gmail.com>
2017-11-17 20:11:32 +0100

    add travis ci

---
+++ travis-build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+docker version
+uname -a
+echo "Updating Docker engine to latest version"
+sudo service docker stop
+curl -fsSL https://get.docker.com/ | sudo sh
+docker version
+
+if [ "$ARCH" == "amd64" ]; then
+  docker build -t ffaerber/appsample .
+fi
+
+if [ "$ARCH" == "arm32v7" ]; then
+  docker build -t ffaerber/appsample --build-arg ARCH=arm32v7 .
+fi
