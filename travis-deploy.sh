0fccf2c35d323e50a6d1e218af3abcffd97c7a59
Felix Faerber <ffaerber@gmail.com>
2017-11-20 16:14:34 +0100

    create tmp on root

--- travis-deploy.sh
+++ travis-deploy.sh
@@ -11,8 +11,8 @@ if [ "$TRAVIS_TAG" ]; then
   docker login -u="$DOCKER_USER" -p="$DOCKER_PASS"
 
   docker run --rm --privileged multiarch/qemu-user-static:register --reset
-  mkdir tmp
-  pushd tmp &&
+  mkdir /tmp
+  pushd /tmp &&
   curl -L -o qemu-arm-static.tar.gz https://github.com/multiarch/qemu-user-static/releases/download/v2.6.0/qemu-arm-static.tar.gz &&
   tar xzf qemu-arm-static.tar.gz &&
   popd
