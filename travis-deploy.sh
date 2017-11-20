d45c3cbdb7a0721874a1478f944a6027759639b5
Felix Faerber <ffaerber@gmail.com>
2017-11-20 11:49:35 +0100

    deploy

---
+++ travis-deploy.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -e
+if [ "$TRAVIS_BRANCH" == "master" && $TRAVIS_TAG ]; then
+  docker version
+  uname -a
+  echo "Updating Docker engine to latest version"
+  sudo service docker stop
+  curl -fsSL https://get.docker.com/ | sudo sh
+  docker version
+
+  docker login -u="$DOCKER_USER" -p="$DOCKER_PASS"
+
+  docker run --rm --privileged multiarch/qemu-user-static:register --reset
+  mkdir tmp
+  pushd tmp &&
+  curl -L -o qemu-arm-static.tar.gz https://github.com/multiarch/qemu-user-static/releases/download/v2.6.0/qemu-arm-static.tar.gz &&
+  tar xzf qemu-arm-static.tar.gz &&
+  popd
+
+  docker build -t ffaerber/appsample:build --build-arg ARCH=$ARCH . --no-cache
+  docker run ffaerber/appsample:build uname -a
+
+  docker tag ffaerber/appsample:build ffaerber/appsample:$TRAVIS_TAG
+  docker tag ffaerber/appsample:build ffaerber/appsample:latest
+  docker push ffaerber/appsample:$TRAVIS_TAG
+  docker push ffaerber/appsample:latest
+fi
