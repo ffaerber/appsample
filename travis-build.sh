58faccb18a769d4b3dd29e5358fa29036d85111d
Felix Faerber <ffaerber@gmail.com>
2017-11-20 11:35:51 +0100

    cleanup

--- travis-build.sh
+++ travis-build.sh
@@ -9,5 +9,10 @@ curl -fsSL https://get.docker.com/ | sudo sh
 docker version
 
 docker run --rm --privileged multiarch/qemu-user-static:register --reset
+mkdir tmp
+pushd tmp &&
+curl -L -o qemu-arm-static.tar.gz https://github.com/multiarch/qemu-user-static/releases/download/v2.6.0/qemu-arm-static.tar.gz &&
+tar xzf qemu-arm-static.tar.gz &&
+popd
 
 docker build -t ffaerber/appsample --build-arg ARCH=$ARCH . --no-cache
