216e5f5119353ad6dc82d627fbd0de05f2b56c81
Felix Faerber <ffaerber@gmail.com>
2017-11-21 11:59:49 +0100

    update qemu

--- travis-deploy.sh
+++ travis-deploy.sh
@@ -13,7 +13,7 @@ if [ "$TRAVIS_TAG" ]; then
   docker run --rm --privileged multiarch/qemu-user-static:register --reset
   mkdir tmp
   pushd tmp &&
-  curl -L -o qemu-arm-static.tar.gz https://github.com/multiarch/qemu-user-static/releases/download/v2.6.0/qemu-arm-static.tar.gz &&
+  curl -L -o qemu-arm-static.tar.gz https://github.com/multiarch/qemu-user-static/releases/download/v2.9.1-1/qemu-arm-static.tar.gz &&
   tar xzf qemu-arm-static.tar.gz &&
   popd
 
