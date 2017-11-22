01efeae02b092a8a3655d10bc7d76d26f12ca637
Felix Faerber <ffaerber@gmail.com>
2017-11-22 18:28:57 +0100

    ci

--- travis-build.sh
+++ travis-build.sh
@@ -10,18 +10,19 @@ docker version
 if [ "$ARCH" != "amd64" ]; then
   # prepare qemu
   docker run --rm --privileged multiarch/qemu-user-static:register --reset
-
   docker create --name qemu-register hypriot/qemu-register
-
   if [ "$ARCH" == "arm" ]; then
     docker cp qemu-register:qemu-arm qemu-arm-static
   fi
-
   if [ "$ARCH" == "arm64" ]; then
     docker cp qemu-register:qemu-aarch64 qemu-aarch64-static
   fi
-
 fi
+
+if [ "$ARCH" == "amd64" ]; then
+  touch fake-qemu-amd64-static
+fi
+
 
 if [ -d tmp ]; then
   docker rm build
