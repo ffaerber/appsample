8c48653587443a8d8ae932fb10feac13745e04ed
Felix Faerber <ffaerber@gmail.com>
2017-11-23 11:23:54 +0100

    ci

--- travis-deploy.sh
+++ travis-deploy.sh
@@ -7,20 +7,6 @@ docker tag appsample "$image:linux-$ARCH-$TRAVIS_TAG"
 docker push "$image:linux-$ARCH-$TRAVIS_TAG"
 
 if [ "$ARCH" == "amd64" ]; then
-  set +e
-  echo "Waiting for other images $image:linux-arm-$TRAVIS_TAG"
-  until docker run --rm stefanscherer/winspector "$image:linux-arm-$TRAVIS_TAG"
-  do
-    sleep 15
-    echo "Try again"
-  done
-  until docker run --rm stefanscherer/winspector "$image:linux-arm64-$TRAVIS_TAG"
-  do
-    sleep 15
-    echo "Try again"
-  done
-  set -e
-
   echo "Downloading manifest-tool"
   wget https://github.com/estesp/manifest-tool/releases/download/v0.5.0/manifest-tool-linux-amd64
   mv manifest-tool-linux-amd64 manifest-tool
