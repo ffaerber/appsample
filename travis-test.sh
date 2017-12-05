924fc678bc581732ed37bbe46f1facb17be40721
Felix Faerber <ffaerber@gmail.com>
2017-12-05 11:18:28 +0100

    ci

--- travis-test.sh
+++ travis-test.sh
@@ -3,5 +3,5 @@ set -e
 
 echo "test with Docker-compose"
 docker-compose up -d mongo-db
-docker-compose run test-app node_modules/.bin/jest
+docker-compose run test-app node_modules/.bin/jest --forceExit
 docker-compose down
